403d7fab55e950807f57f9eb874c19e4d6e863bb
pedrosa <mpedrosa2110@gmail.com>
2023-03-19 20:02:06 -0300

    Proyecto Integrador

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,6 @@ public class Main {
 
         try {
                 for (String linea : Files.readAllLines(dirPartidos)) {
-                    lineaResultados ++;
 
                     arrPartidos = linea.split(";");
 
